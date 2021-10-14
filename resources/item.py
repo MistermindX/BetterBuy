f566ad60db01d5e0b77cb373715b50618d7139c2
MistermindX <max.arenstein@gmail.com>
2021-10-14 15:01:38 -0400

    item resource done

---
+++ resources/item.py
@@ -0,0 +1,46 @@
+from flask import request
+from flask_restful import Resource
+from models.db import db
+from models.item import Item
+from sqlalchemy.orm import joinedload
+
+
+class AllItems(Resource):
+    def get(self):
+        items = Item.find_all()
+        return [item.json() for item in items]
+
+    def post(self):
+        data = request.get_json()
+        item = Item(**data)
+        item.create()
+        return item.json(), 201
+
+
+class ItemDetail(Resource):
+    def get(self, item_id):
+        item = Item.filter_by(id=item_id).first()
+        return item.json()
+
+    def put(self, item_id):
+        data = request.get_json()
+        item = Item.find_by_id(item_id)
+        for key in data:
+            setattr(item, key, data[key])
+        db.session.commit()
+        return item.json()
+
+    def delete(self, item_id):
+        item = Item.find_by_id(item_id)
+        if not item:
+            return {"msg": "Item not found"}, 404
+        db.session.delete(item)
+        db.session.commit()
+        return {"msg": "Item Deleted", "payload": item_id}
+
+
+class ItemsByCategory(Resource):
+    def get(self, category_id):
+        items = Item.query.options(joinedload(
+            'categories')).filter_by(category_id=category_id)
+        return [item.json() for item in items]
