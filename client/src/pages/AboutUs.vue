3eb92e1d928e1a101e4132b2bdee520cc86bbe87
MistermindX <max.arenstein@gmail.com>
2021-10-17 18:20:42 -0400

    updated aboutus css

--- client/src/pages/AboutUs.vue
+++ client/src/pages/AboutUs.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <p>
+  <div class="about-us">
+    <h4>
       Better Buy is a catalog for our ten stores that we have open only in New
       Jersey (for now!) Soon it will have the capability to save orders and for
       you to be able to place orders online for pick up in one of our many
       stores.
-    </p>
+    </h4>
+    <p>More info soon!</p>
   </div>
 </template>
 
