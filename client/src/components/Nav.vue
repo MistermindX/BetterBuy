f843e0aab64b58a1989c5c138d7be3e25588117c
MistermindX <max.arenstein@gmail.com>
2021-10-17 16:31:25 -0400

    added about us page

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/" name="Home">Home</router-link>
     <router-link to="/storelist" name="StoreList">Store List</router-link>
+    <router-link to="/aboutus" name="AboutUs">About Us</router-link>
   </nav>
 </template>
 
--- client/src/router.js
+++ client/src/router.js
@@ -4,6 +4,7 @@ import CategorySearch from './pages/CategorySearch'
 import SignIn from './pages/SignIn'
 import ItemDetail from './pages/ItemDetail'
 import StoreList from './pages/StoreList'
+import AboutUs from './pages/AboutUs'
 
 const routes = [
   { path: '/', component: Home, name: 'Home' },
@@ -14,7 +15,8 @@ const routes = [
   },
   { path: '/signin', component: SignIn, name: 'SignIn' },
   { path: '/items/:item_id', component: ItemDetail, name: 'ItemDetail' },
-  { path: '/storelist', component: StoreList, name: 'StoreList' }
+  { path: '/storelist', component: StoreList, name: 'StoreList' },
+  { path: '/aboutus', component: AboutUs, name: 'AboutUs' }
 ]
 
 export default new VueRouter({ routes, mode: 'history' })
